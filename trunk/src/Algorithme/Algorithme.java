07f02dfafb3d7b37370ee9c4df9f66a0d12d0d0f
shidaDmx <shidaDmx@8f1a06f2-2bc9-11df-9801-9b9f1453ccdf>
2010-03-29 09:16:51 +0000

    Définition de la classe abstraite Algorithme

---
+++ trunk/src/Algorithme/Algorithme.java
@@ -0,0 +1,42 @@
+package Algorithme;
+
+public abstract class Algorithme {
+	private long timeStart;
+	private long timeStop;
+	private boolean isRunning;
+
+	public Algorithme() {
+		timeStart = 0.0;
+		timeStop = 0.0;
+		isRunning = false;
+	}
+
+	public void start() {
+		if (isRunning) {
+			throw new IllegalStateException("L'algorithme est déjà en fonctionement");
+		}
+		timeStop = 0.0;
+		isRunning = true;
+		timeStart = System.currentTimeMillis();
+	}
+
+	public void stop() {
+		if (!isRunning) {
+			throw new IllegalStateException("L'algorightme n'est pas en court de fonctionement");
+		}
+		timeStop = System.currentTimeMillis();
+		isRunning = false;
+	}
+	public boolean getState() {
+		return isRunning;
+	}
+
+	public double getTime() {
+		double time;
+		if (isRunning)
+			time = System.currentTimeMillis() - timeStart;
+		else
+			time = timeStop - timeStart;
+		return time;
+	}
+}
