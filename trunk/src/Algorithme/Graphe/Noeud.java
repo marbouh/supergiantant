9ae6266d8c645d9bdf42fc7d863d2fb35cc7c7b0
shidaDmx <shidaDmx@8f1a06f2-2bc9-11df-9801-9b9f1453ccdf>
2010-03-29 10:44:11 +0000

    Création des classes Noeud et Arrete et amélioration de la classe Graphe

---
+++ trunk/src/Algorithme/Graphe/Noeud.java
@@ -0,0 +1,14 @@
+package Algorithme.Graphe;
+
+public class Noeud {
+	private int id;
+	private String nom; // à voir
+
+	public Noeud(int id) {
+		this.id = id;
+	}
+
+	public int getId() { // le mettre en droit de package
+		return id;
+	}
+}
