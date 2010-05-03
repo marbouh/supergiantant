f1b54eb9fb5d6c65387824c10ab9c8a68570a489
shidaDmx <shidaDmx@8f1a06f2-2bc9-11df-9801-9b9f1453ccdf>
2010-05-03 17:06:07 +0000

    Mise à jour de l'interface

--- trunk/src/Main/MainGenetique.java
+++ trunk/src/Main/MainGenetique.java
@@ -1,11 +1,12 @@
 package Main;
 
 import Algorithme.AlgorithmeGenetique.*;
+import InterfaceGraphique.Interface;
 import Algorithme.Graphe.*;
 
 class MainGenetique {
 	public static void main(String args[]) {
-		GrapheList graphe = new GrapheList("AG", 6);
+		/*		GrapheList graphe = new GrapheList("AG", 10);
 		NoeudList n1 = new NoeudList(1);
 		NoeudList n2 = new NoeudList(2);
 		NoeudList n3 = new NoeudList(3);
@@ -75,8 +76,41 @@ class MainGenetique {
 		graphe.ajouterNoeud(n10);
 
 		//graphe.afficherGraphe();
+		*/
+		GrapheList probleme = new GrapheList("Test",6);
+		NoeudList n1 = new NoeudList(1);
+		NoeudList n2 = new NoeudList(2);
+		NoeudList n3 = new NoeudList(3);
+		NoeudList n4 = new NoeudList(4);
+		NoeudList n5 = new NoeudList(5);
+		NoeudList n6 = new NoeudList(6);
+		
+		n1.addDestination(n2, 4);
+		n1.addDestination(n3, 5);
+		n1.addDestination(n4, 1000);
+		n1.addDestination(n5, 1000);
+		n1.addDestination(n6, 1000);
+		n2.addDestination(n4, 6);
+		n2.addDestination(n5, 1);
+		n2.addDestination(n3, 1000);
+		n2.addDestination(n6, 1000);
+		n3.addDestination(n5, 1);
+		n3.addDestination(n6, 2);
+		n3.addDestination(n4, 1000);
+		n4.addDestination(n5, 2);
+		n4.addDestination(n6, 1000);
+		n6.addDestination(n5, 1);
+		
+		probleme.ajouterNoeud(n1);
+		probleme.ajouterNoeud(n2);
+		probleme.ajouterNoeud(n3);
+		probleme.ajouterNoeud(n4);
+		probleme.ajouterNoeud(n5);
+		probleme.ajouterNoeud(n6);
 
-		AlgoGenetique ag = new AlgoGenetique(20, 100, 25, graphe);
-		ag.resoudre();
+		Interface fenetre = new Interface();
+
+		AlgoGenetique ag = new AlgoGenetique(20, 100, 25, probleme);
+		//		ag.resoudre();
 	}
 }
