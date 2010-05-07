c5ee4b9f29fb2f74e0c31e695157346106358361
shidaDmx <shidaDmx@8f1a06f2-2bc9-11df-9801-9b9f1453ccdf>
2010-05-07 08:51:58 +0000

    Récupération des resultats de l'algorithme génétique fonctionnel et début d'interfaçage avec l'algorithme des fourmis

--- src/InterfaceGraphique/ComposantFourmis.java
+++ src/InterfaceGraphique/ComposantFourmis.java
@@ -1,5 +1,7 @@
 package InterfaceGraphique;
 
+import java.util.ArrayList;
+
 import java.awt.GridLayout;
 
 import javax.swing.JLabel;
@@ -7,18 +9,28 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.BoxLayout;
 
+import Algorithme.AlgorithmeFourmis.AlgoFourmis;
+import Algorithme.Graphe.GrapheList;
+import Algorithme.Graphe.NoeudList;
+
+
 public class ComposantFourmis {
 	private JTextField nbreIterations;
 	private JTextField nbreFourmis;
 	private JTextField vitesseEvapPheromone;
+	private JTextField nbrePheromone;
+	AlgoFourmis af;
 
 	public ComposantFourmis() {
+		af = null;
 		nbreIterations = new JTextField(4);
-		nbreIterations.setText("10");
+		nbreIterations.setText("100");
 		nbreFourmis = new JTextField(4);
-		nbreFourmis.setText("20");
+		nbreFourmis.setText("10");
 		vitesseEvapPheromone = new JTextField(4);
-		vitesseEvapPheromone.setText("0.34");
+		vitesseEvapPheromone.setText("10");
+		nbrePheromone = new JTextField(4);
+		nbrePheromone.setText("0.5");
 	}
 
 	public JPanel creerPanneauParametre() {
@@ -27,19 +39,46 @@ public class ComposantFourmis {
 		contenuParamFourmis.add(new JLabel("Algorithme Fourmis"));
 		JPanel contenuParamFourmisGrid = new JPanel();
 		contenuParamFourmis.add(contenuParamFourmisGrid);
-		contenuParamFourmisGrid.setLayout(new GridLayout(3, 2));
-		contenuParamFourmisGrid.add(new JLabel("Nombre d'itérations"));
+		contenuParamFourmisGrid.setLayout(new GridLayout(4, 2));
+		contenuParamFourmisGrid.add(new JLabel("Nombre d'iterations"));
 		contenuParamFourmisGrid.add(nbreIterations);
 		contenuParamFourmisGrid.add(new JLabel("Nombre de fourmis"));
 		contenuParamFourmisGrid.add(nbreFourmis);
-		contenuParamFourmisGrid.add(new JLabel("Vitesse d'évaporation des phéromonoes"));
+		contenuParamFourmisGrid.add(new JLabel("Vitesse d'evaporation des pheromonoes"));
 		contenuParamFourmisGrid.add(vitesseEvapPheromone);
+		contenuParamFourmisGrid.add(new JLabel("Nombre de pheromone"));
+		contenuParamFourmisGrid.add(nbrePheromone);
 		return contenuParamFourmis;
 	}
 
-	public void lancement(JLabel sortie) {
-		StringBuffer text = new StringBuffer(sortie.getText());
-		text.append("Fourmis\n");
-		sortie.setText(text.toString());
+	public void lancement(GrapheList graphe) {
+		int it = Integer.parseInt(nbreIterations.getText());
+		int fourmis = Integer.parseInt(nbreFourmis.getText());
+		int evap = Integer.parseInt(vitesseEvapPheromone.getText());
+		double phero = Double.parseDouble(nbrePheromone.getText());
+		af = new AlgoFourmis(fourmis, it, evap, phero, graphe);
+		af.traiterProbleme(graphe.getNoeuds().get(0));
 	}
+
+	public int obtenirDistance() {
+		if (af == null) return -1;
+		return af.obtenirDistance();
+	}
+
+	public double obtenirTemps() {
+		if (af == null) return -1;
+		return af.obtenirTemps();
+	}
+
+	public String obtenirParcours() {
+		ArrayList<NoeudList> noeuds = af.obtenirSolution().getNoeuds();
+		String parcours = "";
+		for (int i = 0; i < noeuds.size(); i++) {
+			parcours = parcours + noeuds.get(i).getId();
+			if (i < (noeuds.size() - 1))
+				parcours = parcours + ", ";
+		}
+		return parcours;
+	}
+
 };
--- src/InterfaceGraphique/Interface.java
+++ src/InterfaceGraphique/Interface.java
@@ -25,9 +25,11 @@ public class Interface extends JFrame implements ActionListener
 	JTextField nbreTests;
 	JTable resultat;
 	JLabel resultats;
+	GrapheList graphe;
 
 	public Interface()
 	{
+		recupGraphe();
 		/* Onglet du graphe */
 		JPanel contenuGraphe = new JPanel();
 
@@ -79,10 +81,90 @@ public class Interface extends JFrame implements ActionListener
 	}
 
 	public void actionPerformed(ActionEvent evenement) {
+		ResultTableModele refModele = (ResultTableModele)resultat.getModel();
+		refModele.vider();
 		int nbreTests = Integer.parseInt(this.nbreTests.getText());
 		for (int i = 0; i < nbreTests; i++) {
-			fourmis.lancement(resultats);
-			genetique.lancement(resultats);
+			genetique.lancement(graphe);
+			//			fourmis.lancement(resultats);
+			refModele.ajoutLigne(i + 1, 
+								 genetique.obtenirParcours(), 
+								 genetique.obtenirDistance(), 
+								 (int)genetique.obtenirTemps(),
+								 genetique.obtenirParcours(), 
+								 genetique.obtenirDistance(), 
+								 (int)genetique.obtenirTemps());
 		}
+	}
+	
+	public void recupGraphe() {
+		graphe = new GrapheList("AG", 10);
+		NoeudList n1 = new NoeudList(1);
+		NoeudList n2 = new NoeudList(2);
+		NoeudList n3 = new NoeudList(3);
+		NoeudList n4 = new NoeudList(4);
+		NoeudList n5 = new NoeudList(5);
+		NoeudList n6 = new NoeudList(6);
+		NoeudList n7 = new NoeudList(7);
+		NoeudList n8 = new NoeudList(8);
+		NoeudList n9 = new NoeudList(9);
+		NoeudList n10 = new NoeudList(10);
+
+		n1.addDestination(n2, 4);
+		n1.addDestination(n3, 8);
+		n1.addDestination(n4, 5);
+		n1.addDestination(n5, 6);
+		n1.addDestination(n6, 2);
+		n1.addDestination(n7, 8);
+		n1.addDestination(n8, 3);
+		n1.addDestination(n9, 6);
+		n1.addDestination(n10, 15);
+		n2.addDestination(n3, 3);
+		n2.addDestination(n4, 10);
+		n2.addDestination(n5, 7);
+		n2.addDestination(n6, 1);
+		n2.addDestination(n7, 12);
+		n2.addDestination(n8, 7);
+		n2.addDestination(n9, 6);
+		n2.addDestination(n10, 3);		
+		n3.addDestination(n4, 9);
+		n3.addDestination(n5, 5);
+		n3.addDestination(n6, 8);
+		n3.addDestination(n7, 5);
+		n3.addDestination(n8, 13);
+		n3.addDestination(n9, 16);
+		n3.addDestination(n10, 19);
+		n4.addDestination(n5, 2);
+		n4.addDestination(n6, 10);
+		n4.addDestination(n7, 17);
+		n4.addDestination(n8, 4);
+		n4.addDestination(n9, 11);
+		n4.addDestination(n10, 10);
+		n5.addDestination(n6, 7);
+		n5.addDestination(n7, 18);
+		n5.addDestination(n8, 1);
+		n5.addDestination(n9, 4);
+		n5.addDestination(n10, 9);
+		n6.addDestination(n7, 13);
+		n6.addDestination(n8, 8);
+		n6.addDestination(n9, 7);
+		n6.addDestination(n10, 7);
+		n7.addDestination(n8, 14);
+		n7.addDestination(n9, 9);
+		n7.addDestination(n10, 19);
+		n8.addDestination(n9, 13);
+		n8.addDestination(n10, 3);
+		n9.addDestination(n10, 20);
+
+		graphe.ajouterNoeud(n1);
+		graphe.ajouterNoeud(n2);
+		graphe.ajouterNoeud(n3);
+		graphe.ajouterNoeud(n4);
+		graphe.ajouterNoeud(n5);
+		graphe.ajouterNoeud(n6);
+		graphe.ajouterNoeud(n7);
+		graphe.ajouterNoeud(n8);
+		graphe.ajouterNoeud(n9);
+		graphe.ajouterNoeud(n10);
 	}
 }
